73b18770b0315f314ca8c0a1d7f587c031dbb643
Artemio <artemiolucero@gmail.com>
2022-07-07 19:41:52 -0300

    Agregar archivos de proyecto.

---
+++ UniversityApiBackend/Models/DataModels/Curso.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class Curso: BaseEntity
+    {
+        [Required, StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, StringLength(280)]
+        public string ShortDescription { get; set; } = string.Empty;
+
+        [Required]
+        public string LongDescription { get; set; } = string.Empty;
+
+        [Required]
+        public string TargetAudience { get; set; } = string.Empty;
+
+        [Required]
+        public string Objectives { get; set; } = string.Empty;
+
+        [Required]
+        public string Requirements { get; set; } = string.Empty;
+
+        [Required]
+        public Level level { get; set; } = Level.BASIC;
+        
+    }
+
+    public enum Level
+    {
+        BASIC, INTERMEDIATE, ADVANCED
+    }
+}
