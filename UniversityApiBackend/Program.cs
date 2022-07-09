395a727a578130b41c9667748a264cd729de9578
Artemio <artemiolucero@gmail.com>
2022-07-08 23:09:46 -0300

    Added CORS Policy and Services

--- UniversityApiBackend/Program.cs
+++ UniversityApiBackend/Program.cs
@@ -1,22 +1,39 @@
-//1. Usings to work with Entity Framework
+// 1. Usings to work with Entity Framework
 using Microsoft.EntityFrameworkCore;
 using UniversityApiBackend.DataAccess;
+using UniversityApiBackend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//2. Connection with SQL Server Express
+// 2. Connection with SQL Server Express
 const string CONNECTION_NAME = "UniversityDB";
 var connectionString = builder.Configuration.GetConnectionString(CONNECTION_NAME);
 
-//3. Add Context to Services of builder
+// 3. Add Context to Services of builder
 builder.Services.AddDbContext<UniversityDBContext>(options => options.UseSqlServer(connectionString));
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// 4. Add Custom Services (folder Services)
+builder.Services.AddScoped<IStudentsService, StudentsService>();
+// TODO: Add the rest of services
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// 5. CORS Configuration
+builder.Services.AddCors(options =>
+    {
+        options.AddPolicy(name: "CorsPolicy", builder =>
+        {
+            builder.AllowAnyOrigin();
+            builder.AllowAnyMethod();
+            builder.AllowAnyHeader();
+        });
+});
 
 var app = builder.Build();
 
@@ -32,5 +49,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// 6. Tell app to use CORS
+app.UseCors("CorsPolicy");
 
 app.Run();
