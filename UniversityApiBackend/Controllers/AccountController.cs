79fd18b148061163b12c7003d9eea56dd0b5da97
Artemio <artemiolucero@gmail.com>
2022-07-11 16:29:31 -0300

    Added User Login

---
+++ UniversityApiBackend/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UniversityApiBackend.Helpers;
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+
+        private readonly JwtSettings _jwtSettings;
+
+        public AccountController(JwtSettings jwtSettings)
+        {
+            _jwtSettings = jwtSettings;
+        }
+
+        private IEnumerable<User> Logins = new List<User>()
+        {
+            new User()
+            {
+                Id = 1,
+                Email = "marcia@gmail.com",
+                Name = "Admin",
+                Password = "Admin"
+            },
+            new User()
+            {
+                Id = 2,
+                Email = "ana@gmail.com",
+                Name = "User1",
+                Password = "ana"
+            }
+        };
+
+        [HttpPost]
+        public IActionResult GetToken(UserLogins userLogin)
+        {
+            try
+            {
+                var Token = new UserTokens();
+                var Valid = Logins.Any(user => user.Name.Equals(userLogin.UserName, StringComparison.OrdinalIgnoreCase));
+
+                if (Valid)
+                {
+                    var user = Logins.FirstOrDefault(user => user.Name.Equals(userLogin.UserName, StringComparison.OrdinalIgnoreCase));
+
+                    Token = JwtHelpers.GenTokenKey(new UserTokens()
+                    {
+                        UserName = user.Name,
+                        EmailId = user.Email,
+                        Id = user.Id,
+                        GuiId = Guid.NewGuid(),
+                    }, _jwtSettings);
+                }
+                else
+                {
+                    return BadRequest("Wrong Password");
+                }
+                return Ok(Token);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("GetToken Error", e);
+            }
+        }
+
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator")]
+        public IActionResult GetUsersList()
+        {
+            return Ok(Logins);
+        }
+    }
+}
